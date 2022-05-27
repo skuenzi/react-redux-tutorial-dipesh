780e9008f74cbf4f9cc3720bcb8a7b717e5d81c6
sara.kuenzi <48669509+skuenzi@users.noreply.github.com>
2022-05-27 00:40:36 -0600

    completed app
    followed dipesh malva's video to create app

---
+++ src/containers/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="ui fixed menu">
+            <div className="ui container center">
+                <h2>FakeShop</h2>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header
